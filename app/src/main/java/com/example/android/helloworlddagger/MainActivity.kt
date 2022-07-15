7959fae8a2111c09fd6cb9ea453996e094617eb1
Winsey Li <winsey@storypark.com>
2022-07-16 07:31:12 +1200

    add view binding

--- app/src/main/java/com/example/android/helloworlddagger/MainActivity.kt
+++ app/src/main/java/com/example/android/helloworlddagger/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.android.helloworlddagger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android.helloworlddagger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // View Binding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
